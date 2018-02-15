27cb260890cc0c4dc8fd2fe85ea4af0f2c91f071
Liam <117502469@umail.ucc.ie>
2018-02-15 10:48:39 +0000

    Ebus fix 15/2

---
+++ Interests/ebusiness/Ebus3.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>RECEIPT</title>
+    </head>
+    <body>
+        <h4>RECEIPT</H4>
+        
+        <?php
+        // echo session variables from ebus2
+        echo "Total is " . $_SESSION["total"] . ".";
+        ?>
+    </body>
+</html>    
