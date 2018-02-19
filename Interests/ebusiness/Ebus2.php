0ebaba9120936f062eb4e30d50d3c13050321ba0
Liam <117502469@umail.ucc.ie>
2018-02-19 15:03:19 +0000

    Buttons cloud

--- Interests/ebusiness/Ebus2.php
+++ Interests/ebusiness/Ebus2.php
@@ -11,6 +11,27 @@ session_start();
         <!--jQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript" src="ebus2_validator.js"></script>
+   <style>
+   .success{
+    border: 2px solid #008CBA;
+    background-color: white; 
+    color: black;
+    padding: 10px 16px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+   
+   } 
+   .hover:hover{
+       background-color: #008CBA;
+    color: white;
+   }
+   </style>
     </head>
     
     <body>
@@ -18,15 +39,15 @@ session_start();
         
             <form action="Ebus3.php" method="POST">
                 
-                Name: <input type="text" name="name"> <br>
+                Name: <br/> <input type="text" name="name"> <br/> <br/>
                 
-                E-mail: <input type="text" name="email"> <br>
+                E-mail: <br/> <input type="text" name="email"> <br/> <br/>
                 
-                <label for="user_pin">PIN</label>
+                <label for="user_pin">PIN</label> <br/>
                 
                 <input type="password" id="user_pin" placeholder="Card Pin" maxlength="4">
                
-                <button type="Submit" id="btnPurchase" disabled> Proceed with Purchase </button>
+                <button type="Submit" class="success hover" id="btnPurchase" disabled> Proceed with Purchase </button>
                 
             </form>
             <br />
--- Interests/ebusiness/Ebus3.php
+++ Interests/ebusiness/Ebus3.php
@@ -7,15 +7,15 @@ session_start();
         <title>RECEIPT</title>
     </head>
     <body>
-        <h4>RECEIPT</H4>
+        <h4>Thank you for your purchase.</H4>
         
         <?php
         // echo session variables from ebus2
-        echo "Welcome " . $_POST["name"]; 
+        echo "Thank you: " . $_POST["name"];  
         
-        echo "Your email address is " . $_POST["email"]; 
+        echo "Your recipt will be emailed to: " . $_POST["email"]; 
         
-        echo "Total is " . $_SESSION["total"] . ".";
+        echo "Total is: " . $_SESSION["total"] . ".";
         ?>
     </body>
 </html>    
