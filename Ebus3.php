a6bf991e1314f83b29286b8417ac1947dab70d12
Liam <117502469@umail.ucc.ie>
2018-02-23 11:17:17 +0000

    fixes

--- Ebus3.php
+++ Ebus3.php
@@ -7,13 +7,13 @@ session_start();
         <title>RECEIPT</title>
     </head>
     <body>
-        <h4>Thank you for your purchase.</H4>
+        <H4 style="text-align:center;">Thank you for your purchase.</H4>
         
         <?php
         // echo session variables from ebus2
-        echo "Thank you: " . $_POST["name"];  
+        echo "Thank you: " . $_POST["name"] . "<br/>";  
         
-        echo "Your recipt will be emailed to: " . $_POST["email"]; 
+        echo "Your recipt will be emailed to: " . $_POST["email"] . "<br/>"; 
         
         echo "Total is: " . $_SESSION["total"] . ".";
         ?>
